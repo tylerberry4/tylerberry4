a9ceb0d0c3565e21b5cf61ba9167f0d5f1031fae
Tyler Berry <tyler.berry4@gmail.com>
2014-12-09 03:28:08 -0500

    hero image changes

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+ $(function() {
+
+     $('.hover').sliphover({
+         fontColor: '#D1CA78'
+     });
+     $('.logo').addClass('animated fadeInDownBig');
+     $('.socialLinks').addClass('animated fadeIn');
+     
+ })
