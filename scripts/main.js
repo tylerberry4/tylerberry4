6439344a602d1f51a9d3e7c42df9e49723851d94
Tyler Berry <tyler.berry4@gmail.com>
2014-12-09 04:47:11 -0500

    Added 2nd cards

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,20 @@
      $('.socialLinks').addClass('animated fadeIn');
      
  })
+
+//   (function($) {
+//     var $window = $(window),
+//         $html = $('html');
+
+//     function resize() {
+//         if ($window.width() < 980) {
+//             return $html.addClass('hover');
+//         }
+
+//         $html.removeClass('hover');
+//     }
+
+//     $window
+//         .resize(resize)
+//         .trigger('resize');
+// })(jQuery);
